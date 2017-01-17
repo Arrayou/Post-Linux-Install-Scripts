aaac603affee17d06bced346ac8c9a07379c4178
Callum Smith <dragonsfedora@gmail.com>
2017-01-17 19:02:15 +0000

    Removed yaourt and added brackets

--- Manjaro-Install-Scripts/install.sh
+++ Manjaro-Install-Scripts/install.sh
@@ -2,17 +2,12 @@
 #Title: install.sh
 #Description: This will install google chrome, yaourt and update your system.
 #Author: Dragon's Fedora
-#Date: 27/07/2016
-#Version: 0.0.2
+#Date: 17/01/2017
+#Version: 0.0.3
 #Using: sudo bash install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 sudo pacman -R firefox
-wget http://aur.archlinux.org/packages/yaourt/yaourt.tar.gz
-tar xvzf yaourt.tar.gz
-cd yaourt
-yes | sudo makepkg 
-yes | sudo pacman -U yaourt.pkg.tar.gz -y
-yes | sudo yaourt google-chrome-stable -y
+yes | sudo yaourt google-chrome-stable brackets
 yes | sudo pacman -Syu
 read -n 1 -s -p "Finished!, Press any key to continue"
