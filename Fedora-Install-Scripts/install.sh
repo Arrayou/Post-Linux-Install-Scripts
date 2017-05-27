ca2d1f6cbd20df5eb82f07c17143c68f9c0b1fe9
Callum Smith <dragonsfedora@gmail.com>
2017-05-27 18:20:49 +0100

    added new rpmfusion repo
    RPM fusion repo install now only one-line and updated according to website.

--- Fedora-Install-Scripts/install.sh
+++ Fedora-Install-Scripts/install.sh
@@ -2,14 +2,13 @@
 #Title: install.sh
 #Description: This will add the rpmfusion repositories and install handy applications.
 #Author: Dragon's Fedora
-#Date: 27/07/2016
-#Version: 0.1
-#Using: sudo bash install.sh
+#Date: 27/05/2017
+#Version: 0.3
+#Usage: sudo bash install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 sudo dnf install gnome-tweak-tool -y
-sudo dnf install --nogpgcheck http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-24.noarch.rpm -y
-sudo dnf install --nogpgcheck http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-24.noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo bash -c 'su -c "curl http://folkswithhats.org/fedy-installer -o fedy-installer && chmod +x fedy-installer && ./fedy-installer"' -y
 sudo dnf install icedtea-web java-openjdk -y
 sudo dnf install gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg -y
