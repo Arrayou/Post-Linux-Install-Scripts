22227e5edb4ccb36d2d053fc54467cdfe88c5115
Callum Smith <dragonsfedora@gmail.com>
2017-08-02 18:58:58 +0100

    Removed rpmfusion install as now redundant
    Removed the RPMFusion install as Fedy now installs it by default. Also updated the fedy install command.

--- Fedora-Install-Scripts/install.sh
+++ Fedora-Install-Scripts/install.sh
@@ -8,10 +8,10 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 sudo dnf install gnome-tweak-tool -y
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo bash -c 'su -c "curl http://folkswithhats.org/fedy-installer -o fedy-installer && chmod +x fedy-installer && ./fedy-installer"' -y
+sudo bash sudo sh -c 'curl https://www.folkswithhats.org/installer | bash'
 sudo dnf install icedtea-web java-openjdk -y
 sudo dnf install gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg -y
 sudo dnf install vlc -y
 sudo dnf install wine -y
 sudo dnf update -y
+echo "All Done :)"
