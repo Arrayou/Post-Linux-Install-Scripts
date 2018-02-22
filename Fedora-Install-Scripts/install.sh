7ce3054126f93c55ecb6be8d33a22e4acf04c1d5
Callum Smith <dragonsfedora@gmail.com>
2018-02-22 14:37:11 +0000

    Updated Fedy Commands
    Updated Fedy Commands,
    Added a simple option menu.

--- Fedora-Install-Scripts/install.sh
+++ Fedora-Install-Scripts/install.sh
@@ -3,15 +3,46 @@
 #Description: This will add the rpmfusion repositories and install handy applications.
 #Author: Dragon's Fedora
 #Date: 27/05/2017
-#Version: 0.3
+#Version: 1.0
 #Usage: sudo bash install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-sudo dnf install gnome-tweak-tool -y
-sudo bash sudo sh -c 'curl https://www.folkswithhats.org/installer | bash'
-sudo dnf install icedtea-web java-openjdk -y
-sudo dnf install gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg -y
-sudo dnf install vlc -y
-sudo dnf install wine -y
-sudo dnf update -y
-echo "All Done :)"
+
+PS3='Please enter your choice: '
+options=("Continue" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Continue")
+        echo "#_________________________________________________________________#"
+	echo "#                  Warning! This may Take a while.                #"
+	echo "#_________________________________________________________________#"
+	
+	sudo dnf install gnome-tweak-tool -y
+	
+	echo "#                          Installing Fedy                        #"
+	echo "#_________________________________________________________________#"
+	
+	sudo dnf install https://dl.folkswithhats.org/fedora/$(rpm -E %fedora)/RPMS/folkswithhats-release.noarch.rpm
+	sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+	sudo dnf install fedy
+	
+	echo "#                          Installing Tools                       #"
+	echo "#_________________________________________________________________#"
+	
+	sudo dnf install icedtea-web java-openjdk -y
+	sudo dnf install gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-bad-free gstreamer1-plugins-bad-freeworld gstreamer1-plugins-bad-free-extras ffmpeg -y
+	sudo dnf install vlc -y
+	sudo dnf install wine -y
+	sudo dnf update -y
+	
+	echo "#_________________________________________________________________#"
+	echo "#    All Done :); It's reccomended to reboot your system.         #"
+        echo "#_________________________________________________________________#"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
