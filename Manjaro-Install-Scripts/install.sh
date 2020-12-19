f8120b0607e84ed7faefdd755f5de25f6f2f0edf
Zac <zac1468@gmail.com>
2020-12-19 11:42:33 +0000

    updated manjaro install script

--- Manjaro-Install-Scripts/install.sh
+++ Manjaro-Install-Scripts/install.sh
@@ -6,8 +6,16 @@
 #Version: 0.0.3
 #Using: sudo bash install.sh
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Updated: 19/12/2020
+#Version: 0.0.4
+#Autho: Arrayou
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 sudo pacman -R firefox
 yes | sudo yaourt google-chrome-stable brackets
 yes | sudo pacman -Syu
+yes | sudo pacman -S steam
+yes | sudo pacman -S flameshot 
+yes | sudo pacman -S flatpak
+yes | sudo pacman -S snap
 read -n 1 -s -p "Finished!, Press any key to continue"
